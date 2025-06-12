1307c0c18b5e0be9fb52d7f4e8401a21ee7c983a
ahbisek <abhisek1999rai@gmail.com>
2025-06-12 23:07:25 +0545

    Update main.jsx logic

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import AppRouter from './routers/AppRouters.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import AuthContextProvider from './auth/AuthProvider.jsx'
+import { ToastContainer, Slide } from 'react-toastify'
+
+
+const queryClient = new QueryClient()
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+        <ToastContainer
+          position='top-center'
+          autoClose={2000}
+          hideProgressBar={false}
+          theme='dark'
+          transition={Slide} // Bouce, Slide, Zoom, Flip
+        />
+      </QueryClientProvider>
+    </AuthContextProvider>
+  </StrictMode>,
+)
